ba2737db3d64136cd21124912d8d9362009901b9
manikant <manikant.nitjsr@gmail.com>
2026-07-25 21:20:02 +0530

    implement anomaly to alert and detect error

--- src/main/java/com/api/logging/KafkaProjectApplication.java
+++ src/main/java/com/api/logging/KafkaProjectApplication.java
@@ -1,12 +1,15 @@
 package com.api.logging;
 
+import com.api.logging.config.AnomalyRemediationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(AnomalyRemediationProperties.class) // Enable the new properties class
 public class KafkaProjectApplication {
 
     public static void main(String[] args) {
